f318d038e60afbadd657d62dbf37fbabf5436728
luiizsilverio <luiiz.silverio@gmail.com>
2021-09-29 19:38:29 -0300

    rentals

--- src/__tests__/repositories/RentalsRepositoryInMemory.ts
+++ src/__tests__/repositories/RentalsRepositoryInMemory.ts
@@ -1,4 +1,4 @@
-import { Rental } from "@modules/cars/entities/Rental";
+import { Rental } from "@modules/rentals/entities/Rental";
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 
