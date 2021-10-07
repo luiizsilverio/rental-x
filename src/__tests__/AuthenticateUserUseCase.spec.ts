2a011dccbcb5026548a3a397ac09bf6d51ae7125
luiizsilverio <luiiz.silverio@gmail.com>
2021-10-07 20:15:50 -0300

    ok

--- src/__tests__/AuthenticateUserUseCase.spec.ts
+++ src/__tests__/AuthenticateUserUseCase.spec.ts
@@ -4,16 +4,22 @@ import { ICreateUser } from "@modules/accounts/dtos/ICreateUserDTO"
 import { AuthenticateUserUseCase } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase"
 import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase"
 import { UsersRepositoryInMemory } from "./repositories/UsersRepositoryInMemory"
+import { UsersTokensRepositoryInMemory } from './repositories/UsersTokensRepositoryInMemory'
 
 let authenticateUserUseCase: AuthenticateUserUseCase
 let usersRepositoryInMemory: UsersRepositoryInMemory
 let createUserUseCase: CreateUserUseCase
+let userTokensRepositoryInMemory: UsersTokensRepositoryInMemory
 
 describe("Authenticate user", () => {
 
   beforeEach(() => {
     usersRepositoryInMemory = new UsersRepositoryInMemory()
-    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory)
+    userTokensRepositoryInMemory = new UsersTokensRepositoryInMemory()
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory,
+      userTokensRepositoryInMemory
+    )
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
   })
 
--- src/__tests__/CreateCategoryController.spec.ts
+++ src/__tests__/CreateCategoryController.spec.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata'
 import request from "supertest"
 import { app } from "../app"
 
-
 describe("Create Category Controller", () => {
 
   it("should be able to create a new category", async () => {
@@ -13,8 +12,7 @@ describe("Create Category Controller", () => {
         password: "admin"
       })
 
-    console.log(tokenResp.body)
-    const { token } = tokenResp.body
+    const { refresh_token } = tokenResp.body
 
     const response = await request(app)
       .post("/categories")
@@ -22,7 +20,7 @@ describe("Create Category Controller", () => {
         name: "test",
         description: "test"
       }).set({
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${refresh_token}`
       })
 
       expect(response.status).toBe(201)
